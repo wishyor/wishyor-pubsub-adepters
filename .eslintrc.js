c55157a0a949bb3fd2bf9389a4e2f00ab2ee8f12
himanshu-altioris <himanshu.shanghani@altioris.com>
2025-09-15 13:45:00 +0530

    chore: release v1.0.2 - Friday Release
    - Enhanced NATS adapter stability and performance
    - Added batch publishing and message filtering
    - Improved connection health monitoring
    - Fixed memory leaks and race conditions
    - 15-20% performance improvements across all brokers

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,18 +3,41 @@ module.exports = {
   parserOptions: {
     ecmaVersion: 2020,
     sourceType: 'module',
+    project: './tsconfig.json',
   },
   plugins: ['@typescript-eslint'],
   extends: [
     'eslint:recommended',
+    '@typescript-eslint/recommended',
   ],
   rules: {
     'no-unused-vars': 'off',
+    '@typescript-eslint/no-unused-vars': 'warn',
     'no-console': 'warn',
+    'no-undef': 'off',
   },
   env: {
     node: true,
     es6: true,
+    jest: true,
   },
+  overrides: [
+    {
+      files: ['**/__tests__/**/*', '**/*.test.*'],
+      env: {
+        jest: true,
+      },
+      globals: {
+        jest: 'readonly',
+        describe: 'readonly',
+        it: 'readonly',
+        expect: 'readonly',
+        beforeEach: 'readonly',
+        afterEach: 'readonly',
+        beforeAll: 'readonly',
+        afterAll: 'readonly',
+      },
+    },
+  ],
   ignorePatterns: ['dist/', 'node_modules/', '*.js'],
 };
